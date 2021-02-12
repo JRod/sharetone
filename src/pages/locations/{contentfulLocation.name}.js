6c7461ebcb7bea52692cadc2af15ffa7efa146a0
jrod <josign@tuta.io>
2021-02-12 06:39:12 +0100

    Sprint 2 - Images from Contentful

--- src/pages/locations/{contentfulLocation.name}.js
+++ src/pages/locations/{contentfulLocation.name}.js
@@ -2,36 +2,33 @@ import React from "react"
 import { graphql } from "gatsby"
 import LocationView from "../../views/locationview"
 import Layout from "../../components/layout";
-import {getImage} from "gatsby-plugin-image";
 
 export default function contentfulLocation({data}) {
  const location = data.contentfulLocation
- const imageData = getImage(data.file)
+
 
  return (
    <Layout>
-    <LocationView location={location} image={imageData}  />
+    <LocationView location={location}   />
    </Layout>
  )
 }
 
 export const query = graphql`
-  query($id: String!,$name: String) {
+  query($id: String!) {
     contentfulLocation(id: { eq: $id }) {
       name
       loc {
         lat
         lon
       }
-    }
-    file(name: {eq: $name}) {
-      childImageSharp {
-        gatsbyImageData(
-            width: 600
-            height: 400
-            placeholder: BLURRED
-        )
-      }
+        skyline{
+            gatsbyImageData(
+                width: 600
+                height: 400
+                placeholder: BLURRED
+            )
+        }
     }
   }
 `
--- src/views/locationview.js
+++ src/views/locationview.js
@@ -2,13 +2,13 @@ import * as React from "react"
 
 import {GatsbyImage} from "gatsby-plugin-image";
 import {Heading, Text, Box} from "@chakra-ui/react"
-function LocationView({location,image}) {
-  const {name,loc} = location
+function LocationView({location}) {
+  const {name,loc,skyline} = location
 
   return (
     <>
       <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-        <GatsbyImage image={image} alt={name} layout="constrained"/>
+        <GatsbyImage image={skyline.gatsbyImageData} alt={name} layout="constrained"/>
         <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" ml="2">
           <Text>Latitude: {loc.lat} Longitude: {loc.lon}</Text>
         </Box>
